45509b77e1368a0d2b8e2216d24b0c1e05955af5
GUERSON EMILIA <emiliaguerson@gmail.com>
2022-05-19 15:53:43 -0300

    Ej InputField
    empecé el ej 4 de InputField de la guía de Repaso. Me falta un pedacito

--- Assets/Scripts/VerEnConsola.cs
+++ Assets/Scripts/VerEnConsola.cs
@@ -5,19 +5,32 @@ using UnityEngine.UI;
 
 public class VerEnConsola : MonoBehaviour
 {
-    public Text mensaje;
+    public Text numero1;
+    public Text numero2;
     public Text Msj;
     public void Input()
-    {
-        int num = int.Parse(mensaje.text);
-        if (num < 0)
-        {
-            Debug.Log("Número que no es entero no es válido, reintentar");
-        }
-        else {
-            int res = num * num;
-            Msj.text = res.ToString();
-        }
+    {   
+        int num1 = int.Parse(numero1.text);
+        int num2 = int.Parse(numero2.text);
+        
+            if (num2 == 0)
+            {
+                Debug.Log("El segundo número no debe ser igual a 0, reintentar");
+            }
+            else if (num1 < 0 || num2 < 0)
+            {
+                Debug.Log("Los números ingresados no son números enteros, reintentar");
+            }
+            else if (num1 >= 0 && num2 >= 0)
+            {
+                int res = num1 / num2;
+                Msj.text = res.ToString();
+            }
+            else 
+        //catch(System.FormatException)
+            {
+            Debug.Log("Hay campos vacíos, reintentar");
+            }
         //int counter = 0; counter < int.Parse(cloneAmount.text)
     }
 
