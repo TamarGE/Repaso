f3b4a8af61447f5b5d32a8bb3f980364495f71f5
GUERSON EMILIA <emiliaguerson@gmail.com>
2022-05-23 11:56:49 -0300

    Vector3
    Hice los ejercicios de programación de la guía de repaso de programación, de la categoría Vector3.

---
+++ Assets/Scripts/PlatformScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour
+{
+    public float xSpeed;
+    public float xRightLimit;
+    public float xLeftLimit;
+    public bool ToRight;
+    public GameObject prefab1;
+    public GameObject prefab2;
+
+    void Start()
+    {
+        ToRight = true;
+    }
+
+    void Update()
+    {
+        if (ToRight)//no hace falta poner ==true.
+        {
+            transform.position += new Vector3(xSpeed, 0, 0);
+        }
+        else 
+        { 
+            transform.position -= new Vector3(xSpeed, 0, 0);
+        }
+        if (transform.position.x >= xRightLimit)
+        {
+            ToRight = false;
+            Instantiate(prefab1);
+        }
+        else if (transform.position.x <= xLeftLimit)
+        {
+            ToRight = true;
+            Instantiate(prefab2);
+        }
+
+    }
+}
